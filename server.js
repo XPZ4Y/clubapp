b6f84df985cb0df01c755a515d8a5a74609ceedc
XPZ4Y <xanderquasar@gmail.com>
2025-11-11 23:25:09 +0530

    Initiating backend

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-ECHO is on.
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    // Serve index.html for all routes
+    const filePath = path.join(__dirname, 'index.html');
+    
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>404 - Page Not Found</h1>');
+            return;
+        }
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
